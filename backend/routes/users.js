0bb4f5b21837798d838f5bb54927db103e83221c
simongaviria1 <simongaviria@ac.c4q.nyc>
2018-06-22 11:14:44 -0400

    added routes for API call

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../db/queries')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/events', db.getEvents);
+router.post('/events', db.postEvent);
+router.delete('/events', db.deleteEvent);
 
 module.exports = router;
