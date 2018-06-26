fe0e5912324ed7811ad82060e1a17baf75795393
simongaviria1 <simongaviria@ac.c4q.nyc>
2018-06-26 17:40:26 -0400

    fixed iteration

--- frontend/src/Components/Event.js
+++ frontend/src/Components/Event.js
@@ -1,19 +1,24 @@
 import React, {Component} from 'react'
 import Style from '.././CSS/style.css'
 
-//Component takes in events object and iterates through array of objects to display event details 
+// Component takes in events object and iterates through array of objects to
+// display event details
 
 class Event extends Component {
+
+    handleClick = () => {
+        console.log('clicked')
+    }
+
     render() {
         const {events, day} = this.props
-        console.log('events', events)
+        // console.log('events', events)
         return (
             <div>{events
                     ? <div>{events.map(ele => {
                                 return (
-                                    <div className='event-details'>
+                                    <div className='event-details' onClick={this.handleClick}>
                                         <p>{ele.description}</p>
-                                        <p>{ele.start_time}</p>
                                     </div>
                                 )
                             })}</div>
