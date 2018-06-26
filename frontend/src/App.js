d1294a469d24e04789ee8be40d8762fc819f6b6e
simongaviria1 <simongaviria@ac.c4q.nyc>
2018-06-25 22:50:34 -0400

    added hard coded user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
 import Style from './CSS/style.css'
 
+import axios from 'axios'
+
 import Calendar from './Components/Calendar'
 
 class App extends Component {
+
+  componentWillMount = () => {
+    //Hard coding user to be logged in when someone uses app
+    //Axios requests cannot be made anon
+    axios
+      .post('/users/login', {
+      username: 'simongaviria1',
+      password: 'password'
+    })
+      .then(res => {
+        console.log(res.data.message) //shows whether user logged in
+      })
+  }
   render() {
     return (
       <div className="App">
